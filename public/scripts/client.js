d094f7cff16163a94526cc937941dafe25c99e66
Vohumana <david.linden.james@gmail.com>
2016-07-24 19:57:42 -0700

    Working on the Three.js renderer

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,8 +10,42 @@ socket.on('error', function (error) {
 
 document.addEventListener("DOMContentLoaded", function () {
     var canvas = document.querySelector('canvas');
-    var context = canvas.getContext('2d');
+    var width = 800;
+    var height = 600;
 
+    var scene, camera, renderer;
+    var geometry, material, mesh;
+    
+    init();
+    animate();
+    
+    function init() {
+    
+        scene = new THREE.Scene();
+    
+        camera = new THREE.PerspectiveCamera( 75, width / height, 1, 10000 );
+        camera.position.z = 1000;
+    
+        geometry = new THREE.BoxGeometry( 100, 100, 100 );
+        material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: false } );
+    
+        mesh = new THREE.Mesh( geometry, material );
+        scene.add( mesh );
+    
+        renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: false});
+        renderer.setSize( width, height );
+    }
+    
+    function animate() {
+    
+        requestAnimationFrame( animate );
+    
+        mesh.rotation.x += 0.01;
+        mesh.rotation.y += 0.02;
+    
+        renderer.render( scene, camera );
+    }
+    
     // message method buttons
     var wholeButton = document.querySelector('.whole');
     var streamButton = document.querySelector('.stream');
@@ -24,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
     
     ImageLoader.loadImage(imgUrl)
         .then(function (success) {
-            context.drawImage(success, 0, 0, canvas.width, canvas.height);
+            // context.drawImage(success, 0, 0, canvas.width, canvas.height);
         }, function (error) {
             console.error(error);
         });
